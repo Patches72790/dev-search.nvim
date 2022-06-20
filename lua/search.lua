341a3328eb95a7a755611f7192e20e7e004202cf
Patrick <patches72790@gmail.com>
2022-06-20 11:47:44 -0500

    error handling and added open url command finder

--- lua/search.lua
+++ lua/search.lua
@@ -10,28 +10,17 @@ local init_search = function()
 		return
 	end
 
+	local browser_request_fn = make_browser_request()
+
+	if type(browser_request_fn) == "table" then
+		error("Error creating browser request function: " .. browser_request_fn.message)
+	end
+
 	vim.api.nvim_create_user_command(
 		"DevSearch",
 		make_browser_request,
 		{ desc = "Send a developer search request to default browser" }
 	)
-
-	--	vim.api.nvim_create_user_command("DevSearch", function()
-	--		-- TODO! How to deal with error when executable not found?
-	--
-	--		Job
-	--			:new({
-	--				command = "dev-search",
-	--				args = { vim.fn.input("Search query: ") },
-	--				on_stderr = function(err, data)
-	--					error("Error executing dev-search with message: " .. err)
-	--				end,
-	--				on_stdout = function(err, data)
-	--					error("Error executing dev-search with message: " .. err)
-	--				end,
-	--			})
-	--			:start()
-	--	end, { desc = "Send a call to developer search to local default browser" })
 end
 
 M.setup = function()
--- lua/util.lua
+++ lua/util.lua
@@ -2,21 +2,44 @@ local M = {}
 
 local Job = require("plenary.job")
 
+local find_open_url_command = function()
+	if vim.fn.has("mac") then
+		return "open"
+	elseif vim.fn.has("linux") then
+		return "xdg-open"
+	elseif vim.fn.has("win64") ~= 0 then
+		return "start"
+	else
+		return nil
+	end
+end
+
 M.make_browser_request = function()
 	local url = "https://cse.google.com/cse?cx=c897a4eacb3fd1332"
 	local query = "&q=" .. vim.fn.input("Dev search query: ")
 
-	Job
-		:new({
-			command = "open " .. url .. query,
-			on_stderr = function(err)
-				error("Error executing dev-search with message: " .. err)
-			end,
-			on_stdout = function(err)
-				error("Error executing dev-search with message: " .. err)
-			end,
-		})
-		:start()
+	local url_command = find_open_url_command()
+
+	if url_command == nil then
+		return {
+            error = true,
+            message  = "Error finding open url os command"
+        }
+	end
+
+	return function()
+		Job
+			:new({
+				command = url_command .. url .. query,
+				on_stderr = function(err)
+					error("Error executing dev-search with message: " .. err)
+				end,
+				on_stdout = function(err)
+					error("Error executing dev-search with message: " .. err)
+				end,
+			})
+			:start()
+	end
 end
 
 return M
