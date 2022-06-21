ec0b10f754c7d3aeaaa1c3292409e6e0815cf316
Patrick <patches72790@gmail.com>
2022-06-20 22:07:20 -0500

    fixed condition for open command function

--- lua/util.lua
+++ lua/util.lua
@@ -3,9 +3,9 @@ local M = {}
 local Job = require("plenary.job")
 
 local find_open_url_command = function()
-	if vim.fn.has("mac") then
+	if vim.fn.has("mac") == 0 then
 		return "open"
-	elseif vim.fn.has("linux") then
+	elseif vim.fn.has("linux") == 0 then
 		return "xdg-open"
 	elseif vim.fn.has("win64") ~= 0 then
 		return "start"
